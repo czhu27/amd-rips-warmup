0d2df5dd982e7dd20efba2d6377d750510bd0c18
bhargavsamineni1 <bsamineni8@gmail.com>
2021-06-25 11:36:51 -0700

    Script to search for run errors

---
+++ errorsearch.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import yaml
+
+# Usage: python3 errorsearch.py extErrorBound extErrorBound
+# extErrorBound = Extrapolation error upper bound
+# extErrorBound = Interpolation error upper bound
+
+def find(name, path):
+    res = []
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            res.append(os.path.join(root, name))
+    return res
+
+# Find all errors.yaml files in output folder
+errorPaths = find("errors.yaml", "./output/no_exp_id/")
+goodruns = []
+for path in errorPaths:
+    run = path.split("/")[3]
+    with open(path) as f:
+        errorDict = yaml.safe_load(f)
+        # Compare extrapolation and interpolation error to arguements
+        if float(errorDict['error_ext']) <= float(sys.argv[1]) and float(errorDict['error_int']) <= float(sys.argv[2]):
+            goodruns.append(run)
+
+# Prints run numbers for runs that have lower errror numbers
+for run in goodruns:
+    print(run)

c621f79eeeba28a8c974d316b88ac5780509d1df
czhu27 <czhu27@jhu.edu>
2021-07-13 15:04:26 -0700

    added sample_step

--- wave/forward/example_gaussian.py
+++ wave/forward/example_gaussian.py
@@ -1,7 +1,13 @@
 import sys
+import datetime
 sys.path.append(sys.path[0] + "/app")
+sys.path.append(sys.path[0] + "/../..")
+from data import process_wave_data
 
 from simulator import Simulator
+
+timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+data_dir = "data/wave/" + timestamp
 
 # Create all parameters
 params = {
@@ -15,6 +21,7 @@ params = {
 	# Time-related parameters
 	"tf": 1.0,			# final time
 	"dt": 0.01,			# time step
+	"sample_step": 0.04,# amount of time between samples, should be multiple of dt
 	"show_every": 10,	# interval between two time steps reports
 	"integrator": 'rk2',# time integrator 
 	# Discretization-related parameters
@@ -22,10 +29,13 @@ params = {
 	"pm": 0,			# polynomial degree for material properties	
 	"int_diag": 3,		# integration used for diagnostics (num 1D Gauss)  
 	# Initialization function
-	"initializer": 'gaussian'
+	"initializer": 'gaussian',
+	"data_dir": "data/wave/" + timestamp
 }	
 
 # Create simulator and run
 simulator = Simulator(params)
 simulator.run()
 simulator.finalize()
+
+process_wave_data(data_dir, params)

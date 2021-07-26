7ef32a5de3f602a9640595db6aac14ca6015a8a3
David <dndavini2.71@gmail.com>
2021-07-26 22:50:07 +0000

    don't be greedy?

--- train.py
+++ train.py
@@ -188,6 +188,18 @@ def train(configs: Configs):
 	else:
 		raise ValueError("Unknown device " + configs.device)
 
+	# Memory allocation
+	if configs.be_greedy:
+		for i in range(5):
+			print("I'M GREEDY. SHHHHH DON'T TELL MOM...")
+	else:
+		physical_devices = tf.config.list_physical_devices('GPU')
+		try:
+			tf.config.experimental.set_memory_growth(physical_devices[0], True)
+		except:
+			# Invalid device or cannot modify virtual devices once initialized.
+			pass
+
 	# Setup folder structure vars
 	output_dir = configs.output_dir
 	# TB logs
